c07e038387f529403b8762d76c99a1ffe926ac21
Supakrit Nithikethkul <supakrit.nit@dome.tu.ac.th>
2023-03-25 21:25:08 +0700

    hit

--- cartrack/api/views.py
+++ cartrack/api/views.py
@@ -33,9 +33,9 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     
 class VDOViewSet(ModelViewSet):
-    #queryset = VDO.objects.all()
+    queryset = VDO.objects.all()
     serializer_class = VDOSerializer
-    #permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
     http_method_names = ['post', ]
 
     def create(self, request, *args, **kwargs):
@@ -54,9 +54,64 @@ class VDOViewSet(ModelViewSet):
 class StatusViewSet(ReadOnlyModelViewSet):
     queryset = TaskResult.objects.all()
     serializer_class = StatusSerializer
-    #permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
     http_method_names = ['post','get']
     def retrieve(self, request,pk=None):
         ts = TaskResult.objects.get_task(pk)
         sts = StatusSerializer(ts)
         return Response(sts.data)
+
+
+# @api_view(['GET'])
+# def get_download_txt(request, job_id):
+#     # check if job exists and retrieve its status from the databas
+
+#     # return the status in the response
+#     return Response({'status': 'txt'}, status=status.HTTP_200_OK)
+
+# @api_view(['GET'])
+# def get_download_vid(request, job_id):
+#     # check if job exists and retrieve its status from the database
+    
+
+#     # return the status in the response
+#     return Response({'status': 'vid'}, status=status.HTTP_200_OK)
+
+# @api_view(['GET'])
+# def get_job_status(request, job_id):
+#     # check if job exists and retrieve its status from the database
+#     try:
+#         job = Job.objects.get(id=job_id)
+#     except Job.DoesNotExist:
+#         return Response({'error': 'Job not found.'}, status=status.HTTP_404_NOT_FOUND)
+    
+#     status = job.status
+
+#     # return the status in the response
+#     return Response({'status': status}, status=status.HTTP_200_OK)
+
+
+# @api_view(['POST'])
+# def create_file(request):
+#     # Get the JSON payload from the request
+#     data = request.data.get('json_data')
+#     if not data:
+#         return Response({'error': 'JSON data is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+#     # Get the file upload from the request
+#     file = request.data.get('file')
+#     if not file:
+#         return Response({'error': 'File upload is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+#     # Save the JSON payload to a file
+#     with open(os.path.join('path/to/folder', 'data.json'), 'w') as f:
+#         f.write(data)
+
+#     # Save the file upload to a file
+#     with open(os.path.join('path/to/folder', file.name), 'wb') as f:
+#         for chunk in file.chunks():
+#             f.write(chunk)
+
+#     #call the job scheduler 
+#     res = add.delay(2,2)
+#     return Response({'success': f'JSON payload and file upload saved successfully. {res.id}'}, status=status.HTTP_201_CREATED)
